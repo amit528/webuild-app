88976dbdd878b8346dddd3bfc7f6d2119b2a2ae4
amit528 <amit528>
2021-06-07 21:27:49 +0530

    testing

--- dist/webuild-app.e31bb0bc.js
+++ dist/webuild-app.e31bb0bc.js
@@ -38494,7 +38494,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44803" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46501" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
